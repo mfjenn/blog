2cfb2350924b9eb426b988b85741a26f3f09a3fa
Mary Jenn <mary@maryfjenn.com>
2012-10-16 20:33:18 -0700

    pushing to origin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120706205651) do
+ActiveRecord::Schema.define(:version => 20120727003906) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(:version => 20120706205651) do
   end
 
   add_index "comments", ["post_id"], :name => "index_comments_on_post_id"
+
+  create_table "donations", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.float    "amount"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "name"
